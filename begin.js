7d805b4b6cd867753dad09ae82cce8b67e064166
AyushhhCR7 <ayushmessi2006@gmail.com>
2025-12-24 22:22:01 +0530

    add js files

---
+++ begin.js
@@ -0,0 +1,12 @@
+console.log("hello guys");
+let a = "10";
+let name= "john";
+console.table({a, name})
+console.log(typeof a);
+let valueInNumber=Number(a)
+console.log(typeof valueInNumber); /*input ko as a NUMBER hi accept karega*/
+let num=10;
+console.log(num);
+for(let i=0;i<10;i++){
+    console.log(i);
+}
