bb3d798324ea179252a25182003ab5f54224679f
AyushhhCR7 <ayushmessi2006@gmail.com>
2025-12-25 16:10:09 +0530

    slicing and I/O

--- begin2.js
+++ begin2.js
@@ -1,3 +1,9 @@
+const str="Hello World";
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.substring(0,7));
+
+
 console.log("hello guys");
 const readline=require("readline");
 const r1=readline.createInterface({
@@ -5,6 +11,7 @@ const r1=readline.createInterface({
     output: process.stdout
 });
 r1.question("Enter a number: ", function(a){
-    console.log(a);
+    console.log(`the number you eneterd is ${a} ....thank you!`);
     r1.close();
 });
+
