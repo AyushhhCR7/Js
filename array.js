2b4c534c0eb093e77aa6202fa78369efac0c7ca5
AyushhhCR7 <ayushmessi2006@gmail.com>
2025-12-28 16:15:50 +0530

    arrays and its functions...thode se

---
+++ array.js
@@ -0,0 +1,28 @@
+// const arr=[1, 2, 3, 4, 5];
+// console.log(arr);
+
+//const arr2=new Array(6, 7, 8, 9, 10);
+// console.log(arr[3]);
+// console.log(`the array on 4th number is ${arr[3]}`);
+
+// console.log(arr2);
+
+// arr2.push(11);
+
+// console.log(arr2);
+// arr2.unshift(0);
+// console.log(arr2);
+// console.log(arr2.includes(5)); //check whether its prestn or not
+
+a=[10,20,30,40,50];
+b=[60,70,80,90,100];
+// a.push(b)
+// console.log(a);
+// console.log(typeof a);
+
+const x=a.concat(b)
+console.log(x);
+
+
+n1=[1,2,3,[4,5],6,[7,8,9]];
+console.log(n1.flat()); //sabko ek saath kar dega
