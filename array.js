0cf38b0ff188fe5b9a2ade9550e27029496fcdff
AyushhhCR7 <ayushmessi2006@gmail.com>
2025-12-30 22:46:19 +0530

    arrays and its functions...thode se

--- array.js
+++ array.js
@@ -14,15 +14,26 @@
 // console.log(arr2);
 // console.log(arr2.includes(5)); //check whether its prestn or not
 
-a=[10,20,30,40,50];
-b=[60,70,80,90,100];
+// a=[10,20,30,40,50];
+// b=[60,70,80,90,100];
 // a.push(b)
 // console.log(a);
 // console.log(typeof a);
 
-const x=a.concat(b)
-console.log(x);
+// const x=a.concat(b)
+// console.log(x);
 
 
-n1=[1,2,3,[4,5],6,[7,8,9]];
-console.log(n1.flat()); //sabko ek saath kar dega
+// n1=[1,2,3,[4,5],6,[7,8,9]];
+// console.log(n1.flat()); //sabko ek saath kar dega
+
+
+
+
+//khela hoga ab arrays ke saath...
+const a={
+    name:"ayush",
+    age:19,
+    location:"kolkata"
+}
+console.log(a);
