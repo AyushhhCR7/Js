80727f313800ee491678aab0444895b7808c83fb
AyushhhCR7 <ayushmessi2006@gmail.com>
2026-01-09 21:38:01 +0530

    arrow functions

---
+++ arrow.js
@@ -0,0 +1,10 @@
+const add=(a, b) => {
+    return a + b;
+};
+console.log(add(5,2));
+//curly bracekt use kar rahe hain to return keyword use karenge
+
+//another way of using arrow funciton...
+// no need of using return keyword
+ const additon=(c,d)=> c+d;
+console.log(additon(10,15));
