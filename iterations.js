1da0da43e36f3f5157fb6072b0a164b57ebce1da
AyushhhCR7 <ayushmessi2006@gmail.com>
2026-01-11 22:24:19 +0530

    more

--- iterations.js
+++ iterations.js
@@ -1,8 +1,31 @@
- for(let i=0; i<10; i++) {
-    //  console.log(`outer loop has ${i}`);
-    console.log(`table of ${i}`);
-     for(let j=0; j<=10; j++) {
-        // console.log(`   inner loop has ${j}`);
-        console.log(`${i} * ${j} = ${i*j}`);
-     }
+//  for(let i=0; i<10; i++) {
+//     //  console.log(`outer loop has ${i}`);
+//     console.log(`table of ${i}`);
+//      for(let j=0; j<=10; j++) {
+//         // console.log(`   inner loop has ${j}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+//      }
+// }
+
+// const myarr=['apple', 'banana', 'cherry'];
+// for(const i in myarr) {
+//    console.log(`value is ${myarr[i]}`);
+
+//    if (myarr.includes('banana')) {
+//       console.log('banana found');
+//       break;
+//    }
+// }
+const a="ayush";
+for(const i in a){
+   console.log(a[i]);
+}
+//BOTH ARE SAME THING
+const b= "AYUSH";
+for(const char of b){
+   if(char==='Y'){
+      continue;
+   }    console.log(char);
+
+
 }
