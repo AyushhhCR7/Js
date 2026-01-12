1e97c4e72c0ecf97e47d1cf6dbbdb7b42c32414e
AyushhhCR7 <ayushmessi2006@gmail.com>
2026-01-12 15:08:10 +0530

    arrays with loops

--- map.js
+++ map.js
@@ -1,26 +1,58 @@
-const map=new Map();
-map.set(1,"AYUSH");
-map.set(2,"KUMAR");
-map.set(3,"Boxing");
-// console.log(map);
-// for(const i of map){
+// const map=new Map();
+// map.set(1,"AYUSH");
+// map.set(2,"KUMAR");
+// map.set(3,"Boxing");
+// // console.log(map);
+// // for(const i of map){
+// //     console.log(i);
+// // }
+
+// // for(const [key, value] of map){
+// //     console.log(key,`----`, value);
+// // }
+
+// const a={
+//     name:"Ayush",
+//     age:21,
+//     type:"Boxer"
+// }
+// console.log(a);
+// console.log(Object.keys(a));
+// // for(const [key, value] of Object.entries(a)){
+// //     console.log(key,`----`, value);
+// // }
+// for(const key in a){
+//     console.log(key);
+// }
+
+const myarr=["RONALDO", "BIVOL", "MESSI", "CANELO"];
+// for(const i in myarr){
+//     console.log(myarr[i]);
+// }
+// for(const i of myarr){
 //     console.log(i);
 // }
+// myarr.forEach(function(value){
+//     console.log(value);
+// })
 
-// for(const [key, value] of map){
-//     console.log(key,`----`, value);
-// }
+// myarr.forEach((value)=>{
+//     console.log(value);
+// })
 
-const a={
-    name:"Ayush",
-    age:21,
-    type:"Boxer"
-}
-console.log(a);
-console.log(Object.keys(a));
-// for(const [key, value] of Object.entries(a)){
-//     console.log(key,`----`, value);
-// }
-for(const key in a){
-    console.log(key);
-}
+const mycoding=[
+    {
+        name:"Ayush",
+        age:"twenty",
+        type:"Boxer"
+    },
+    {
+    },
+    {                          //what we are doing here is...
+                               //creating an array of objects
+                               //and accessing elemetns from each objects
+    },
+]
+mycoding.forEach((value)=>{
+    console.log(value.name);
+})
